04bc8be34bc522c96f12f6c0f7e5a93b82666b71
李家辉 <1743951794@qq.com>
2026-05-12 15:43:44 +0800

    创建项目的基本框架

---
+++ src/main/java/com/onik/flowticket/FlowTicketApplication.java
@@ -0,0 +1,13 @@
+package com.onik.flowticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onik/flowticket/FlowTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onik.flowticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
